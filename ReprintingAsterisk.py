6c1e48d8a1bc1ba48d8b118acfaf4e8f718fe02f
Robbie-Wadud <amaan.wadud@gmail.com>
2020-10-10 02:07:03 -0500

    Program to print asterisks

---
+++ ReprintingAsterisk.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 26 20:37:51 2020
+
+@author: Robbi
+"""
+
+#For loop to do the 3.18 program
+for asterisk in range(1, 11):
+    print('*' * asterisk, ' ' ,'*' * (11 - asterisk) , ' ', '*' * (11 - asterisk), ' ', '*' * asterisk)
